e1a13fe5f0ba2335dbfe88ffff7322d0a357e848
me <me@Pascal-Koko>
2024-11-18 11:33:38 +0200

    add new attributes to customer Object and define getters and setters

--- src/main/java/dev/ucbc/customerTest/Customer.java
+++ src/main/java/dev/ucbc/customerTest/Customer.java
@@ -3,4 +3,38 @@ package dev.ucbc.customerTest;
 public class Customer {
 
 	private String customerCode;
+	private String fullName;
+	private String telephone;
+	
+	public Customer(String customerCode, String fullName, String telephone) {
+		this.customerCode = customerCode;
+		this.fullName = fullName;
+		this.telephone = telephone;
+	}
+
+	public String getCustomerCode() {
+		return customerCode;
+	}
+
+	public void setCustomerCode(String customerCode) {
+		this.customerCode = customerCode;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	
 }
